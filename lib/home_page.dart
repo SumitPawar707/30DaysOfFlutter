5259f67dbaa605fcf0c64697a8ee8c7dc15fe556
Sumit Pawar <sp260089@gmail.com>
2024-12-22 19:30:21 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  final int days = 30;
+  final String name = "codepur";
+
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF42A5F5),
+        centerTitle: true,
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days of flutter by $name"),
+        ),
+      ),
+      drawer: const Drawer(),
+    );
+  }
+}
