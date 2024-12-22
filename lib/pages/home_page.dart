f49c0b1f47000c7415065931a430aa9553b7914a
Sumit Pawar <sp260089@gmail.com>
2024-12-22 21:46:56 +0530

    day 3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xFF42A5F5),
+        //backgroundColor: const Color(0xFF42A5F5),
         centerTitle: true,
         title: Text("Catelog App"),
       ),
